a32b05d91d1f12f20e88f9acc8f10a2a4314154a
F1903 <osman@DESKTOP-61N0P7R.localdomain>
2025-12-06 03:12:02 +0300

    Add corner camel.gif logo and animated loper text

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,15 @@ import UploadPage from "./pages/UploadPage";
 import "./App.css";
 import { WalletConnection } from "./components/WalletConnection";
 import "./App.css";
+import "./logo.css"; // <-- ekledik
 
 function App() {
   return (
     <AppProviders>
+      <div className="corner-logo">
+        <img src="/camel.gif" alt="camel" className="camel-gif" />
+        <span className="loper-text">loper</span>
+      </div>
       <UploadPage>
       </UploadPage>
       <WalletConnection></WalletConnection>
