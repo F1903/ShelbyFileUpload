f1e1a28564ea9f1eb458a79b84ed3577045139ef
F1903 <osman@DESKTOP-61N0P7R.localdomain>
2026-01-09 15:26:47 +0300

    UI Update

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -2,18 +2,33 @@
 
 import { FileUpload } from "@/components/FileUpload";
 import { WalletConnection } from "@/components/WalletConnection";
+import "./uploadPage.css";
 
 export default function UploadPage() {
   return (
-    <div>
-      <header>
-        <div>
-          <h1>Shelby File Upload</h1>
+    <div className="page">
+      <header className="header">
+        <div className="headerLeft">
+          <h1 className="title">Shelby File Upload</h1>
+          <p className="subtitle">
+            Faucet yap, Cüzdanını bağla, dosyanı seç ve Shelby ağına yükle. <h2>Petra wallet &#187; ayarlar &#187; network &#187; SHELBYNET. </h2>
+          </p>
+        </div>
+
+        <div className="headerRight">
           <WalletConnection />
         </div>
       </header>
-      <main>
-        <FileUpload />
+
+      <main className="main">
+        <div className="card">
+          <FileUpload />
+        </div>
+
+        <p className="hint">
+          <a href= "https://docs.shelby.xyz/apis/faucet/shelbyusd">USD FAUCET</a> <br></br>
+          <a href= "https://docs.shelby.xyz/apis/faucet/aptos">APT FAUCET </a>
+        </p>
       </main>
     </div>
   );
